7d9ea115f11ecd45b91f32d3ac5718a2eb5866cf
virex <539401@syscom.com.tw>
2026-09-04 14:09:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,136 +1,178 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+# ------------------------------------------------------------
+# Resource Group
+# 此資源不在原始圖片中，但所有圖片內資源都必須隸屬於 Resource Group。
+# ------------------------------------------------------------
+
+resource "azurerm_resource_group" "hub" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
-  location = var.resource_group_location
+# ------------------------------------------------------------
+# 1. Hub-VNET
+# ------------------------------------------------------------
+
+resource "azurerm_virtual_network" "hub" {
+  name                = "Hub-VNET"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+  address_space       = var.hub_vnet_address_space
+
+  tags = var.tags
 }
 
-resource "random_pet" "azurerm_virtual_network_name" {
-  prefix = "vnet"
+# ------------------------------------------------------------
+# Azure Bastion 必要子網路
+# 名稱必須使用 AzureBastionSubnet
+# ------------------------------------------------------------
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = var.bastion_subnet_prefixes
 }
 
-resource "azurerm_virtual_network" "test" {
-  name                = random_pet.azurerm_virtual_network_name.id
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# ------------------------------------------------------------
+# VPN Gateway 必要子網路
+# 名稱必須使用 GatewaySubnet
+# ------------------------------------------------------------
+
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = var.gateway_subnet_prefixes
 }
 
-resource "random_pet" "azurerm_subnet_name" {
-  prefix = "sub"
+# ------------------------------------------------------------
+# 3. hub-vnet-bastion-IPaddress
+# Bastion Basic 使用 Standard、Static Public IP。
+# ------------------------------------------------------------
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "hub-vnet-bastion-IPaddress"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+  ip_version        = "IPv4"
+
+  tags = var.tags
 }
 
-resource "azurerm_subnet" "test" {
-  name                 = random_pet.azurerm_subnet_name.id
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# ------------------------------------------------------------
+# 2. Hub-VNET-Bastion
+# ------------------------------------------------------------
 
-resource "azurerm_public_ip" "test" {
-  name                = "publicIPForLB"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_lb" "test" {
-  name                = "loadBalancer"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  frontend_ip_configuration {
-    name                 = "publicIPAddress"
-    public_ip_address_id = azurerm_public_ip.test.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "test" {
-  loadbalancer_id = azurerm_lb.test.id
-  name            = "BackEndAddressPool"
-}
-
-resource "azurerm_network_interface" "test" {
-  count               = 2
-  name                = "acctni${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_bastion_host" "hub" {
+  name                = "Hub-VNET-Bastion"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+  sku                 = var.bastion_sku
 
   ip_configuration {
-    name                          = "testConfiguration"
-    subnet_id                     = azurerm_subnet.test.id
+    name                 = "bastion-ip-configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+
+  tags = var.tags
+}
+
+# ------------------------------------------------------------
+# 7. VPNGateway-IPaddress
+# ------------------------------------------------------------
+
+resource "azurerm_public_ip" "vpn_gateway" {
+  name                = "VPNGateway-IPaddress"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+  ip_version        = "IPv4"
+
+  tags = var.tags
+}
+
+# ------------------------------------------------------------
+# 6. VPNGateway
+# 傳統 VNet-based Virtual Network Gateway
+# ------------------------------------------------------------
+
+resource "azurerm_virtual_network_gateway" "vpn" {
+  name                = "VPNGateway"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  type          = "Vpn"
+  vpn_type      = "RouteBased"
+  active_active = false
+  enable_bgp    = var.enable_bgp
+  sku           = var.vpn_gateway_sku
+  generation    = "Generation2"
+
+  ip_configuration {
+    name                          = "vpn-gateway-ip-configuration"
+    public_ip_address_id          = azurerm_public_ip.vpn_gateway.id
     private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_availability_set" "avset" {
-  name                         = "avset"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name          = azurerm_resource_group.rg.name
-  platform_fault_domain_count  = 2
-  platform_update_domain_count = 2
-  managed                      = true
-}
-
-resource "random_pet" "azurerm_linux_virtual_machine_name" {
-  prefix = "vm"
-}
-
-resource "azurerm_linux_virtual_machine" "test" {
-  count                 = 2
-  name                  = "${random_pet.azurerm_linux_virtual_machine_name.id}${count.index}"
-  location              = azurerm_resource_group.rg.location
-  availability_set_id   = azurerm_availability_set.avset.id
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.test[count.index].id]
-  size                  = "Standard_D2s_v3"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+    subnet_id                     = azurerm_subnet.gateway.id
   }
 
-  #admin_ssh_key {
-  #  username   = var.username
-  #  public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
-  #}
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    name                 = "myosdisk${count.index}"
-  }
-
-  computer_name  = "hostname"
-  admin_username = var.username
-  admin_password = "Syscom2025Dev"
-  disable_password_authentication = false
+  tags = var.tags
 }
 
-resource "azurerm_managed_disk" "test" {
-  count                = 2
-  name                 = "datadisk_existing_${count.index}"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1024"
+# ------------------------------------------------------------
+# 4. LocalGateway
+# 代表地端 FortiGate 與地端網段
+# ------------------------------------------------------------
+
+resource "azurerm_local_network_gateway" "fortigate" {
+  name                = "LocalGateway"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  gateway_address = var.onprem_vpn_public_ip
+  address_space   = var.onprem_address_spaces
+
+  tags = var.tags
 }
 
-resource "azurerm_virtual_machine_data_disk_attachment" "test" {
-  count              = 2
-  managed_disk_id    = azurerm_managed_disk.test[count.index].id
-  virtual_machine_id = azurerm_linux_virtual_machine.test[count.index].id
-  lun                = "10"
-  caching            = "ReadWrite"
+# ------------------------------------------------------------
+# 5. ToFortiVPN
+# Azure VPN Gateway 至地端 FortiGate 的 S2S IPsec Connection
+# ------------------------------------------------------------
+
+resource "azurerm_virtual_network_gateway_connection" "fortigate" {
+  name                = "ToFortiVPN"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  type = "IPsec"
+
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.vpn.id
+  local_network_gateway_id   = azurerm_local_network_gateway.fortigate.id
+
+  shared_key = var.vpn_shared_key
+
+  enable_bgp = var.enable_bgp
+
+  # 若 FortiGate 有指定 IKE/IPsec Proposal，可取消下面區塊註解，
+  # 並依 FortiGate Phase 1 / Phase 2 設定調整。
+  #
+  # ipsec_policy {
+  #   dh_group         = "DHGroup14"
+  #   ike_encryption   = "AES256"
+  #   ike_integrity    = "SHA256"
+  #   ipsec_encryption = "AES256"
+  #   ipsec_integrity  = "SHA256"
+  #   pfs_group        = "PFS14"
+  #   sa_datasize      = 102400000
+  #   sa_lifetime      = 27000
+  # }
+
+  tags = var.tags
 }
